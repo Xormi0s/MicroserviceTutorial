9f5f1ef424a164f719abf1cc530cf5aad90f97fa
Jonas <xormios@hotmail.com>
2026-05-15 08:27:08 +0200

    Insight-service init

---
+++ insight-service/src/test/java/com/xormios/insight_service/InsightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xormios.insight_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
